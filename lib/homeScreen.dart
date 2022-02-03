1ca4198be53f06495889e043bee636e0a29b7639
Vichhka <office.vichhka@gmail.com>
2022-02-03 13:36:05 +0700

    test list item

--- lib/homeScreen.dart
+++ lib/homeScreen.dart
@@ -3,10 +3,9 @@ import 'dart:developer';
 import 'package:flutter/material.dart';
 import 'package:tnampet/medicine.dart';
 
-class HomeScreen extends StatefulWidget
-{
+class HomeScreen extends StatefulWidget {
   List<Medicine> medicinelist = [];
-  HomeScreen(List<Medicine> medicinelist){
+  HomeScreen(List<Medicine> medicinelist) {
     this.medicinelist = medicinelist;
   }
 
@@ -14,15 +13,16 @@ class HomeScreen extends StatefulWidget
   _HomeState createState() => _HomeState(medicinelist);
 }
 
-class _HomeState extends State<HomeScreen>{
+class _HomeState extends State<HomeScreen> {
   List<Medicine> medicinelist = [];
   TextEditingController editingController = TextEditingController();
   final duplicateItems = List<String>.generate(10000, (i) => "Item $i");
   var items = [];
   // bool _isLoading = false;
 
-  _HomeState(List<Medicine> medicinelist){
+  _HomeState(List<Medicine> medicinelist) {
     this.medicinelist = medicinelist;
+    this.items.addAll(duplicateItems);
   }
 
   // void getMedicinefromAPI() async
@@ -46,10 +46,10 @@ class _HomeState extends State<HomeScreen>{
   void filterSearchResults(String query) {
     List<String> dummySearchList = [];
     dummySearchList.addAll(duplicateItems);
-    if(query.isNotEmpty) {
+    if (query.isNotEmpty) {
       List<String> dummyListData = [];
       dummySearchList.forEach((item) {
-        if(item.contains(query)) {
+        if (item.contains(query)) {
           dummyListData.add(item);
         }
       });
@@ -64,48 +64,49 @@ class _HomeState extends State<HomeScreen>{
         items.addAll(duplicateItems);
       });
     }
-
   }
 
   @override
   Widget build(BuildContext context) => Scaffold(
-    backgroundColor: Colors.white38,
-    body: Container(
-      alignment: Alignment.center,
-      child: (medicinelist.isEmpty) ? CircularProgressIndicator() : Container(
-        child: Column(
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: TextField(
-                onChanged: (value) {
-                  filterSearchResults(value);
-                },
-                controller: editingController,
-                decoration: InputDecoration(
-                    labelText: "Search",
-                    hintText: "Search",
-                    prefixIcon: Icon(Icons.search),
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(Radius.circular(25.0)))),
-              ),
-            ),
-            Expanded(
-              child: ListView.separated(
-                shrinkWrap: true,
-                itemCount: items.length,
-                itemBuilder: (context, index) {
-                  return ListTile(
-                    title: Text('${items[index]}'),
-                  );
-                },
-                separatorBuilder: (context, index) => Divider(),
-              ),
-            ),
-          ],
+        backgroundColor: Colors.white38,
+        body: Container(
+          alignment: Alignment.center,
+          child: (medicinelist.isEmpty)
+              ? CircularProgressIndicator()
+              : Container(
+                  child: Column(
+                    children: <Widget>[
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: TextField(
+                          onChanged: (value) {
+                            filterSearchResults(value);
+                          },
+                          controller: editingController,
+                          decoration: InputDecoration(
+                              labelText: "Search",
+                              hintText: "Search",
+                              prefixIcon: Icon(Icons.search),
+                              border: OutlineInputBorder(
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(25.0)))),
+                        ),
+                      ),
+                      Expanded(
+                        child: ListView.separated(
+                          shrinkWrap: true,
+                          itemCount: items.length,
+                          itemBuilder: (context, index) {
+                            return ListTile(
+                              title: Text('${items[index]}'),
+                            );
+                          },
+                          separatorBuilder: (context, index) => Divider(),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
         ),
-      ),
-    ),
-  );
-
+      );
 }
