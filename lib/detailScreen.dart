3f7b2c434702f3992ed282bb266de61a1ea6dc0b
Vichhka <office.vichhka@gmail.com>
2022-02-04 16:20:40 +0700

    design detail page

--- lib/detailScreen.dart
+++ lib/detailScreen.dart
@@ -1,24 +1,61 @@
 import 'package:flutter/material.dart';
 
-class DetailScreen extends StatefulWidget {
-  var detail;
+import 'medicine.dart';
 
-  DetailScreen(item) {
-    this.detail = item;
+class DetailScreen extends StatefulWidget {
+  var item;
+
+  DetailScreen(Medicine item) {
+    this.item = item;
   }
 
   @override
-  _DetailScreen createState() => _DetailScreen();
+  _DetailScreen createState() => _DetailScreen(item);
 }
 
 class _DetailScreen extends State<DetailScreen> {
+  var item;
+  var _isfavorite = false;
+  _DetailScreen(item) {
+    this.item = item;
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar:AppBar(
+      appBar: AppBar(
         backgroundColor: Colors.orange,
-        title: Text("ព័ត៍មានលំអិត",textAlign: TextAlign.center, )
-      )
+        title: Text(
+          "ព័ត៍មានលំអិត",
+          textAlign: TextAlign.center,
+        ),
+      ),
+      body: ListView(
+          scrollDirection: Axis.vertical,
+          padding: EdgeInsets.all(8),
+          children: [
+            Row(children: [
+              Text(
+                item.title,
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 14,
+                  fontFamily: 'JetBrainsMono',
+                ),
+              ),
+              Spacer(),
+              IconButton(
+                onPressed: () {
+                  setState(() {
+                    this._isfavorite = (_isfavorite) ? false : true;
+                  });
+                },
+                icon: Icon(
+                  Icons.favorite,
+                  color: (_isfavorite) ? Colors.red : Colors.black,
+                ),
+              ),
+            ]),
+          ]),
     );
   }
 }
--- lib/splashScreen.dart
+++ lib/splashScreen.dart
@@ -4,28 +4,26 @@ import 'package:flutter/material.dart';
 import 'mainScreen.dart';
 import 'medicine.dart';
 
-class SplashScreen extends StatefulWidget
-{
+class SplashScreen extends StatefulWidget {
   @override
   _SplashScreenState createState() => _SplashScreenState();
 }
 
 class _SplashScreenState extends State<SplashScreen> {
-
   List<Medicine> medicinelist = [];
 
-  void getMedicinefromAPI() async
-  {
+  void getMedicinefromAPI() async {
     MedicineAPI.getCharacters().then((response) {
       setState(() {
         Iterable list = json.decode(response.body);
         medicinelist = list.map((e) => Medicine.fromJson(e)).toList();
+        medicinelist.sort(
+            (a, b) => a.title.toLowerCase().compareTo(b.title.toLowerCase()));
         Navigator.of(context).pushReplacement(
             MaterialPageRoute(builder: (context) => MainScreen(medicinelist)));
       });
     });
   }
-
 
   @override
   void initState() {
@@ -41,13 +39,19 @@ class _SplashScreenState extends State<SplashScreen> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Image.asset('lib/assets/images/logo.png',height: 150,),
-            SizedBox(height: 10,),
-            CircularProgressIndicator(valueColor: AlwaysStoppedAnimation<Color>(Colors.orange),)
+            Image.asset(
+              'lib/assets/images/logo.png',
+              height: 150,
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            CircularProgressIndicator(
+              valueColor: AlwaysStoppedAnimation<Color>(Colors.orange),
+            )
           ],
         ),
       ),
-
     );
   }
 }
